51477138e454997a81c2675fe7f77767a13e3904
markhewspawood-spec <markhews.pawood@gmail.com>
2026-01-14 17:49:50 +0000

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,14 +1,13 @@
-import * as React from 'react';
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./styles.css"; // or index.css
 
-import App from './App';
-
-const rootElement = document.getElementById('root');
-const root = createRoot(rootElement);
+const container = document.getElementById("root") as HTMLElement;
+const root = createRoot(container);
 
 root.render(
-  <StrictMode>
+  <React.StrictMode>
     <App />
-  </StrictMode>
+  </React.StrictMode>
 );
